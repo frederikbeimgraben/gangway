d7ee3f1fbbc56a16dde8eab1a1e45a9113ae3a92
Frederik Beimgraben <frederik@beimgraben.net>
2026-01-16 14:55:02 +0100

    PWA-support

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,13 +4,33 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+    applicationName: "Gangway",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "default",
+        title: "Gangway",
+    },
     title: "GANGWAY Control",
     description: "LED Controller Interface",
-    icons: {
-        icon: "/favicon.png",
-        icon: "/favicon.ico",
-        icon: "/favicon.svg",
+    formatDetection: {
+        telephone: false,
     },
+    icons: {
+        shortcut: "/favicon.ico",
+        apple: "/icons/apple-touch-icon.png",
+        icon: [
+            { url: "/favicon.png" },
+            { url: "/favicon.ico" },
+            { url: "/favicon.svg" },
+        ],
+    },
+    manifest: "/manifest.json",
+};
+
+export const viewport = {
+    themeColor: "#000000",
+    viewport:
+        "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover",
 };
 
 export default function RootLayout({ children }) {
